ddc53db72f756baaf208c67e57344ef3adc2984f
ajacinto137 <ajacinto137>
2020-04-10 23:13:32 -0400

    updated models and migrations

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+# go to terminal
+# must rum make migrations after to update migrations file
+# python manage.py sqlmigrate blog 0001 (name of app and file app)
+#  then run python manage.py migrate
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
